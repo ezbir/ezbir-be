ca8c47a9e6a989a07df984697edb0f0d2011d5e9
Ruslan Gren <49315740+RuslanGren@users.noreply.github.com>
2024-07-27 13:33:31 +0300

    disable cors configuration (#9)

--- src/main/java/com/ua/ezbir/web/controllers/PostController.java
+++ src/main/java/com/ua/ezbir/web/controllers/PostController.java
@@ -11,7 +11,6 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/post")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class PostController {
     private final PostService postService;
 
